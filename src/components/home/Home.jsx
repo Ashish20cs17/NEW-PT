d22c595cccbc3a34ffa11642701fc73f10adc6af
Ashish20cs17 <aak479900@gmail.com>
2025-05-13 17:37:13 +0530

    Your commit message

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -31,12 +31,16 @@ function Home() {
           width: 100%;
           margin: 0 auto;
           background-color: #fff;
+         background-image: radial-gradient(circle, #c8bebe 1px, transparent 1px);
+          background-size: 20px 20px; /* Adjust this value to control the size of the dots */
         }
+
         @media (max-width: 991px) {
           .landing-container {
             max-width: 991px;
           }
         }
+
         @media (max-width: 640px) {
           .landing-container {
             max-width: 640px;
